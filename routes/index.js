73f558f72cc4d4db7d5b35d9b1340630cf3f14c1
Stefan Zeuch <stefanzeuch12@gmail.com>
2021-07-27 23:04:26 +0200

    feat: enterpriseNo v2 adjustments

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,7 @@ router.get('/enterpriseName/:name', async function(req, res, next) {
   (async () => {
     try {
       const browser = await puppeteer.launch({
-        headless: false,
-        // args: [
-        //   "--no-sandbox",
-        // ],
+        headless: false
       });
       console.log('browser launched...')
       const page = await browser.newPage();
@@ -42,6 +39,12 @@ router.get('/enterpriseName/:name', async function(req, res, next) {
       
       await page.click("input[name='ctl00$cntMain$btnLogin']");
       console.log('clicked login...');
+
+      await page.waitForTimeout(1000);
+
+      // Validate logged in
+      await page.waitForSelector("#cntMain_Updatepanel1");
+      console.log('logged in...');
 
       await page.waitForTimeout(1000);
 
@@ -123,15 +126,46 @@ router.get('/enterpriseNo/:number*', async function(req, res, next) {
   (async () => {
     try {
       const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          "--no-sandbox",
-        ],
+        headless: false
       });
       console.log('browser launched...')
       const page = await browser.newPage();
+      await page.setViewport({ width: 1920, height: 1080});
       await page.goto(URL);
-      console.log('page opened...')
+      console.log('page opened...');
+
+      await page.waitForSelector("input[name='ctl00$cntMain$txtIDNo']");
+      console.log('found login...');
+
+      await page.waitForSelector("#cntMain_chkId_ToggleButton");
+
+      await page.waitForTimeout(2500);
+
+      await page.click("#cntMain_chkId_ToggleButton");
+      console.log('toggled...');
+
+      await page.waitForTimeout(1000);
+
+      await page.focus("input[name='ctl00$cntMain$txtCustCode']")
+      await page.keyboard.type(process.env.USERNAME);
+
+      await page.focus("input[name='ctl00$cntMain$txtPassword']")
+      await page.keyboard.type(process.env.PASSWORD);
+      
+      await page.click("input[name='ctl00$cntMain$btnLogin']");
+      console.log('clicked login...');
+
+      await page.waitForTimeout(1000);
+
+      // Validate logged in
+      await page.waitForSelector("#cntMain_Updatepanel1");
+      console.log('logged in...');
+
+      await page.waitForTimeout(1000);
+
+      await page.goto('https://bizportal.gov.za/bizprofile.aspx');
+
+      await page.waitForTimeout(1000);
 
       await page.waitForSelector("select[name='ctl00$cntMain$drpSearchOptions']");
       console.log('found search options component...')
@@ -146,21 +180,37 @@ router.get('/enterpriseNo/:number*', async function(req, res, next) {
 
       console.log('entered "'+joined+'"...')
 
-      await page.click("#ctl00_cntMain_lnkSearchIcon");
+      await page.click("input[name='ctl00$cntMain$btnSearch']");
 
-      console.log('clicked search...')
+      console.log('clicked search...');
 
-      // TODO: Look for any errors
+      await page.waitForTimeout(1000);
+
+      await page.waitForSelector("#cntMain_lblError", {
+        timeout: 120000
+      });
+
+      await page.waitForTimeout(1000);
+
+      await page.click("input[name='ctl00$cntMain$btnSearch']");
 
       console.log('waiting for search results box...');
 
-      await page.waitForSelector("#ctl00_cntMain_pnlEntNoSearch", {
+      await page.waitForTimeout(1000);
+
+      await page.waitForSelector("#cntMain_pnlResults", {
         timeout: 120000
       });
 
       console.log('found search results box...');
 
-      const html = await page.evaluate(el => el.innerHTML, await page.$('#ctl00_cntMain_TabContainer1'));
+      const companyDetailsTab = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(1) .tab-content'));
+      const directorDetailsTab = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(2) .tab-content'));
+      const annualReturnDetailsTable = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(3) .tab-content'));
+      const outstandingAnnualReturnsTable = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(3) .tab-content div:nth-of-type(2)'));
+      const enterpriseHistoryTab = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(4) .tab-content'));
+      // const beeTab = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(5) .tab-content'));
+      // const otherDetailsTab = await page.evaluate(el => el.innerHTML, await page.$('#cntMain_pnlResults > div:nth-child(6) .tab-content'));
 
       console.log('got lookup results...');
 
@@ -168,51 +218,41 @@ router.get('/enterpriseNo/:number*', async function(req, res, next) {
 
       console.log('closed browser...');
 
-      const dom = new JSDOM(html);
-      const enterpriseDetails = dom.window.document.querySelector("#ctl00_cntMain_TabContainer1_TabPanel1 tbody");
-
-      const enterpriseRows = enterpriseDetails.rows;
+      const companyDetailsDom = new JSDOM(companyDetailsTab);
+      const sections = companyDetailsDom.window.document.querySelectorAll("section");
 
       const companyDetails = {};
 
-      for (let index = 0; index < enterpriseRows.length; index++) {
-        if (index > 0) {
-          const row = enterpriseRows[index];
+      for (let index = 0; index < sections.length; index++) {
+        const section = sections[index];
 
-          if (index > 0 && index < 7) {
-            const cells = Array.prototype.slice.call(row.cells);
-            const key = cells[0].innerHTML.replace(/(\r\n|\n|\r)/gm, "").trim();
-            const value = cells[1].innerHTML.trim().replace(/(<([^>]+)>)/gi, "");
-            companyDetails[camelize(key)] = value;
-          }
-          // Addresses
-          if (index === 7) {
-            const cells = Array.prototype.slice.call(row.cells);
-            const addresses = new JSDOM(cells[0].innerHTML);
-            const rows = addresses.window.document.querySelector("table tr");
-            const physicalDOM = new JSDOM(rows.cells[0].innerHTML);
-            const postalDOM = new JSDOM(rows.cells[1].innerHTML);
-            const physical = physicalDOM.window.document.querySelector("span").innerHTML.split('<br>');
-            const postal = postalDOM.window.document.querySelector("span").innerHTML.split('<br>');
-            companyDetails['physicalAddress'] = physical;
-            companyDetails['postalAddress'] = postal;
-          }
+        const sectionArray = Array.prototype.slice.call(section.children)
+
+        if (index < 6) {
+          const key = sectionArray[0].innerHTML.replace(/(\r\n|\n|\r)/gm, "").trim();
+          const value = sectionArray[1].innerHTML.trim().replace(/(<([^>]+)>)/gi, "");
+          companyDetails[camelize(key)] = value;
+        }
+        if (index === 6) {
+          const physicalDom = new JSDOM(sectionArray[1].innerHTML);
+          const physical = physicalDom.window.document.querySelector("span").innerHTML.split('<br>');
+          companyDetails['physicalAddress'] = physical;
+        }
+        if (index === 7) {
+          const postalDom = new JSDOM(sectionArray[1].innerHTML);
+          const postal = postalDom.window.document.querySelector("span").innerHTML.split('<br>');
+          companyDetails['postalAddress'] = postal;
         }
       }
 
-      const directorDetailsTable = dom.window.document.querySelector("#ctl00_cntMain_TabContainer1_TabPanel2 tbody");
-
-      const directorOuterRows = directorDetailsTable.rows[1];
-
-      const cells = Array.prototype.slice.call(directorOuterRows.cells);
-
-      const directorRows = Array.prototype.slice.call(cells[0].getElementsByTagName('tr'));
+      const directorDetailsDom = new JSDOM(directorDetailsTab);
+      const directorDetailsTableRows = directorDetailsDom.window.document.querySelectorAll("tbody tr");
 
       const directors = [];
 
-      for (let index = 0; index < directorRows.length; index++) {
+      for (let index = 0; index < directorDetailsTableRows.length; index++) {
         if (index > 0) {
-          const element = directorRows[index];
+          const element = directorDetailsTableRows[index];
           const cells = element.cells;
           const id = cells[0].innerHTML;
           const names = cells[1].innerHTML;
@@ -230,28 +270,23 @@ router.get('/enterpriseNo/:number*', async function(req, res, next) {
       }
 
       companyDetails['directors'] = directors;
-      
-      const annualReturnDetailsTable = dom.window.document.querySelector("#ctl00_cntMain_TabContainer1_TabPanel3 tbody table tbody");
 
-      const annualReturnDetailsRows = annualReturnDetailsTable.rows;
+      const annualReturnDetailsDom = new JSDOM(annualReturnDetailsTable);
+      const annualReturnDetailsTableRows = annualReturnDetailsDom.window.document.querySelector("tbody");
 
       const filedAnnualReturns = [];
 
-      for (let index = 0; index < annualReturnDetailsRows.length; index++) {
+      for (let index = 0; index < annualReturnDetailsTableRows.rows.length; index++) {
         if (index > 0) {
-          const element = annualReturnDetailsRows[index];
+          const element = annualReturnDetailsTableRows.rows[index];
           const cells = element.cells;
           if (cells && cells.length > 1) {
             const arYear = cells[0].innerHTML;
-            const customerCode = cells[1].innerHTML;
-            const amountPaid = cells[2].innerHTML;
-            const trackingNumber = cells[3].innerHTML;
-            const dateFiled = cells[4].innerHTML;
+            const amountPaid = cells[1].innerHTML;
+            const dateFiled = cells[2].innerHTML;
             filedAnnualReturns.push({
               arYear,
-              customerCode,
               amountPaid,
-              trackingNumber,
               dateFiled
             })
           }
@@ -260,18 +295,16 @@ router.get('/enterpriseNo/:number*', async function(req, res, next) {
 
       companyDetails['filedAnnualReturns'] = filedAnnualReturns;
 
-      const outstandingAnnualReturnsTable = dom.window.document.querySelector("#ctl00_cntMain_TabContainer1_TabPanel3 table:nth-of-type(2) table tbody");
-
-      const outstandingAnnualReturnsRows = outstandingAnnualReturnsTable.rows;
+      const outstandingAnnualReturnsDom = new JSDOM(outstandingAnnualReturnsTable);
+      const outstandingAnnualReturnsTableRows = outstandingAnnualReturnsDom.window.document.querySelector("tbody");
 
       const outstandingAnnualReturns = [];
 
-      for (let index = 0; index < outstandingAnnualReturnsRows.length; index++) {
+      for (let index = 0; index < outstandingAnnualReturnsTableRows.rows.length; index++) {
         if (index > 0) {
-          const element = outstandingAnnualReturnsRows[index];
+          const element = outstandingAnnualReturnsTableRows.rows[index];
           const cells = element.cells;
           const arYear = cells[0].innerHTML;
-          // TODO: double check with a company with outstanding annual returns
           if (arYear !== 'This enterprise does not have any outstanding annual returns at the moment') {
             const arMonth = cells[1].innerHTML;
             const arNonComplianceDate = cells[2].innerHTML;
@@ -286,15 +319,14 @@ router.get('/enterpriseNo/:number*', async function(req, res, next) {
 
       companyDetails['outstandingAnnualReturns'] = outstandingAnnualReturns;
 
-      const enterpriseHistoryTable = dom.window.document.querySelector("#ctl00_cntMain_TabContainer1_TabPanel4 tbody table tbody");
-
-      const enterpriseHistoryRows = enterpriseHistoryTable.rows;
+      const enterpriseHistoryDom = new JSDOM(enterpriseHistoryTab);
+      const enterpriseHistoryTableRows = enterpriseHistoryDom.window.document.querySelectorAll("tbody tr");
 
       const enterpriseHistory = [];
 
-      for (let index = 0; index < enterpriseHistoryRows.length; index++) {
+      for (let index = 0; index < enterpriseHistoryTableRows.length; index++) {
         if (index > 0) {
-          const element = enterpriseHistoryRows[index];
+          const element = enterpriseHistoryTableRows[index];
           const cells = element.cells;
           const date = cells[0].innerHTML;
           const details = cells[1].innerHTML;
